29b7a3540000b9797bda92bdae0cbe4c4fb30aa3
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2024-11-12 11:43:35 -0300

    fix sum

--- MMAS/ant.py
+++ MMAS/ant.py
@@ -64,7 +64,7 @@ class Ant:
             # Mark the current node as visited
             self.visited_nodes.add(self.current_node)
             self.path_cost += self.graph_api.get_edge_cost(self.current_node, next_node)
-            self.limit_load_current_vehicle =+ self.graph_api.get_demand_node(next_node)
+            self.limit_load_current_vehicle += self.graph_api.get_demand_node(next_node)
             self.current_node = next_node
 
     def _choose_next_node(self) -> int | str:

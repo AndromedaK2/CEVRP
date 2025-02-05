01fba2417f4c34bb4bf13d61d29405339035a328
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2025-02-04 21:23:35 -0300

    add - config file

---
+++ Shared/config.py
@@ -0,0 +1,27 @@
+# config.py
+from typing import List
+
+INSTANCE_FILES: List[str] = [
+    "Shared/Instances/E-n22-k4.evrp",
+    "Shared/Instances/E-n23-k3.evrp",
+    "Shared/Instances/E-n30-k3.evrp",
+    "Shared/Instances/E-n33-k4.evrp",
+    "Shared/Instances/E-n51-k5.evrp",
+    "Shared/Instances/E-n76-k7.evrp",
+    "Shared/Instances/E-n101-k8.evrp",
+    "Shared/Instances/X-n143-k7.evrp",
+    "Shared/Instances/X-n214-k11.evrp",
+    "Shared/Instances/X-n351-k40.evrp",
+    "Shared/Instances/X-n459-k26.evrp",
+    "Shared/Instances/X-n573-k30.evrp",
+    "Shared/Instances/X-n685-k75.evrp",
+    "Shared/Instances/X-n749-k98.evrp",
+    "Shared/Instances/X-n819-k171.evrp",
+    "Shared/Instances/X-n916-k207.evrp",
+    "Shared/Instances/X-n1001-k43.evrp"
+]
+
+DEFAULT_SOURCE_NODE: str = "1"
+NUM_ANTS: int = 100
+MAX_ANT_STEPS: int = 100
+NUM_ITERATIONS: int = 100
--- main.py
+++ main.py
@@ -4,35 +4,14 @@ from ALNS_METAHEURISTIC.destroy_operators import random_destroy
 from ALNS_METAHEURISTIC.make_alns import make_alns
 from ALNS_METAHEURISTIC.repair_operators import greedy_repair
 from ALNS_METAHEURISTIC.solution_state import CevrpState
+from Shared.config import INSTANCE_FILES, DEFAULT_SOURCE_NODE, NUM_ANTS, MAX_ANT_STEPS, NUM_ITERATIONS
 from Shared.path import Path
 from Shared.coordinates_demand_manager import CoordinatesDemandManager
 from MMAS.aco import ACO
 from Shared.cevrp import CEVRP
 
-# Constant for instance files
-INSTANCE_FILES: List[str] = [
-    "Shared/Instances/E-n22-k4.evrp",
-    "Shared/Instances/E-n23-k3.evrp",
-    "Shared/Instances/E-n30-k3.evrp",
-    "Shared/Instances/E-n33-k4.evrp",
-    "Shared/Instances/E-n51-k5.evrp",
-    "Shared/Instances/E-n76-k7.evrp",
-    "Shared/Instances/E-n101-k8.evrp",
-    "Shared/Instances/X-n143-k7.evrp",
-    "Shared/Instances/X-n214-k11.evrp",
-    "Shared/Instances/X-n351-k40.evrp",
-    "Shared/Instances/X-n459-k26.evrp",
-    "Shared/Instances/X-n573-k30.evrp",
-    "Shared/Instances/X-n685-k75.evrp",
-    "Shared/Instances/X-n749-k98.evrp",
-    "Shared/Instances/X-n819-k171.evrp",
-    "Shared/Instances/X-n916-k207.evrp",
-    "Shared/Instances/X-n1001-k43.evrp"
-]
-DEFAULT_SOURCE_NODE: str = "1"
-NUM_ANTS: int = 100
-MAX_ANT_STEPS: int = 100
-NUM_ITERATIONS: int = 100
+
+
 
 def select_instance(instance_files: List[str]) -> str:
     """Prompts the user to select an instance file."""

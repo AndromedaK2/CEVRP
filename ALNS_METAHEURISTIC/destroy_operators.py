2fe10d29e927485fc72f56b6e7a3b1323747b76a
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2025-02-26 17:20:29 -0300

    refactor - destroy operator

--- ALNS_METAHEURISTIC/destroy_operators.py
+++ ALNS_METAHEURISTIC/destroy_operators.py
@@ -21,66 +21,70 @@ def remove_overcapacity_nodes(state: CevrpState, rnd_state: Optional[np.random.R
     state_copy = state.copy()
     paths = state_copy.paths
     energy_capacity = state_copy.cevrp.energy_capacity
-    charging_stations = state_copy.cevrp.charging_stations
+    charging_stations = set(state_copy.cevrp.charging_stations)
 
     if len(paths) < 2:
         raise ValueError("At least two paths are required to perform this operation.")
 
-    paths_copy = [path.copy() for path in paths]
-    unassigned = state_copy.unassigned
+    unassigned = set(state_copy.unassigned)
     new_paths = []  # Store valid paths
+    energy_cache = {}
 
-    for path in paths_copy:
+    for path in paths:
+        nodes = path.nodes
+        num_nodes = len(nodes)
         # **Remove paths that contain only the depot and one customer**
-        if len(path.nodes) <= 3:
-            unassigned.extend([node for node in path.nodes if node != DEFAULT_SOURCE_NODE])
+        if num_nodes <= 3:
+            unassigned.update(node for node in nodes if node != DEFAULT_SOURCE_NODE)
             continue
 
         energy_consumption = 0
-        valid_path = Path()
-        valid_path.nodes.append(path.nodes[0])
+        valid_nodes = [nodes[0]]
 
-        for i in range(len(path.nodes) - 1):
-            current_node, next_node = path.nodes[i], path.nodes[i + 1]
+        for i in range(num_nodes - 1):
+            current_node, next_node = nodes[i], nodes[i + 1]
 
             # **Reset energy at charging stations**
             if current_node in charging_stations:
                 energy_consumption = 0  # Fully recharge
 
             # Compute additional energy needed to reach the next node
-            additional_energy = state_copy.get_edge_energy_consumption(current_node, next_node)
+            edge = (current_node, next_node)
+            if edge not in energy_cache:
+                energy_cache[edge] = state_copy.get_edge_energy_consumption(current_node, next_node)
+            additional_energy = energy_cache[edge]
+
 
             # **Check if the vehicle can safely reach the next node**
             if energy_consumption + additional_energy > energy_capacity:
+                valid_nodes.pop(-1)
                 break  # Stop before exceeding capacity
 
             # Add the node only if it doesn't exceed capacity
-            valid_path.nodes.append(next_node)
+            valid_nodes.append(next_node)
             energy_consumption += additional_energy
 
-        # **Handle unassigned nodes properly**
-        remaining_nodes = path.nodes[len(valid_path.nodes):]  # Nodes that couldn't be added
-
-        if remaining_nodes:
-            # **Filter out charging stations from unassigned nodes**
-            unassigned.extend([node for node in remaining_nodes if node != DEFAULT_SOURCE_NODE and node not in charging_stations])
+        # Nodes that couldn't be added
+        remaining_nodes = nodes[len(valid_nodes):]
+        # **Filter out charging stations from unassigned nodes**
+        unassigned.update(node for node in remaining_nodes if node != DEFAULT_SOURCE_NODE and node not in charging_stations)
 
         # **Recalculate cost, demand, and energy**
-        if valid_path.nodes:
-            valid_path.path_cost = state_copy.graph_api.calculate_path_cost(valid_path.nodes)
-            valid_path.demand = state_copy.graph_api.get_total_demand_path(valid_path.nodes)
+        if len(valid_nodes) > 1:
+            valid_path = Path()
+            valid_path.nodes = valid_nodes
+            valid_path.path_cost = state_copy.graph_api.calculate_path_cost(valid_nodes)
+            valid_path.demand = state_copy.graph_api.get_total_demand_path(valid_nodes)
             valid_path.energy = energy_consumption
-
-        if len(valid_path.nodes) > 2 and valid_path.nodes[0] == DEFAULT_SOURCE_NODE and valid_path.nodes[-1] == DEFAULT_SOURCE_NODE:
-            valid_path.feasible = True
-        else:
-            valid_path.feasible = False
-        new_paths.append(valid_path)
+            valid_path.feasible = (
+                valid_nodes[0] == DEFAULT_SOURCE_NODE
+                and valid_nodes[-1] == DEFAULT_SOURCE_NODE
+                and len(valid_nodes) > 2
+            )
+            new_paths.append(valid_path)
 
     # **Filter out charging stations from the final unassigned list**
-    unassigned = [node for node in unassigned if node not in charging_stations]
-    rnd_state.shuffle(unassigned)
-    rnd_state.shuffle(new_paths)
+    unassigned = list(unassigned - charging_stations)
     state_copy.graph_api.visualize_graph(new_paths, charging_stations, state_copy.cevrp.name)
     return CevrpState(new_paths, unassigned, state_copy.graph_api, state_copy.cevrp)
 
